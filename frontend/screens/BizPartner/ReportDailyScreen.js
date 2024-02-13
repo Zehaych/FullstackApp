d28be4486463a411693a29393ee488c1d3420565
yl <ongyenlay98@gmail.com>
2024-02-14 01:28:27 +0800

    Adjust progress bar in Summary Weekly & Monthly

--- frontend/screens/BizPartner/ReportDailyScreen.js
+++ frontend/screens/BizPartner/ReportDailyScreen.js
@@ -208,7 +208,9 @@ const styles = StyleSheet.create({
   },
   subtitle: { 
     fontSize: 16, 
-    marginBottom: 5 
+    marginBottom: 5,
+    textAlign: "right",
+    width: "60%",
   },
   subtitle1: {
     fontSize: 16,
